e8b43696a79ab17e7cb1ceae05fe942bf66285e7
Carlos Manuel Ojeda Hernández <Carlos_moh@outlook.com>
2025-02-10 23:27:26 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import {View, Text, StyleSheet} from 'react-native';
+
+
+const App = () => {
+  
+  return(
+    //Aqui se llaman todos los componentes a mostrar
+    <Text> Hola</Text>
+  )
+}
+
+export default App;
