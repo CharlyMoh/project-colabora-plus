7d4a76fd1c4acf26ea1a83d7b8b4d9755b429e5d
AldoMisaelLealPalafox <aldolealpalafox@hotmail.com>
2025-02-11 21:19:26 -0600

    es necesario el commit aqui?

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import {View, Text, StyleSheet} from 'react-native';
-
-
+import React from 'react';
+//import { View, Text, StyleSheet } from 'react-native';
+import ProyectosScreen from './src/screens/HomeScreen';
+import TareasScreen from './src/screens/TaskScreenUser';
+import AgregarIntegrantesScreen from './src/screens/ProjectScreen2';
+import ConfiguracionScreen from './src/screens/Settings'
 const App = () => {
-  
-  return(
-    //Aqui se llaman todos los componentes a mostrar
-    <Text> Hola</Text>
-  )
-}
+  return (
+    //<ProyectosScreen />
+    //<TareasScreen/>
+    <AgregarIntegrantesScreen />
+    
+    //<ConfiguracionScreen/>
+  );
+};
 
 export default App;
