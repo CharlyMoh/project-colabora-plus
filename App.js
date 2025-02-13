444ccadb44bf408445e8f7b19dd50cbcad138ec7
Carlos Manuel Ojeda Hernández <Carlos_moh@outlook.com>
2025-02-12 18:30:41 -0600

    App

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import {View, Text, StyleSheet} from 'react-native';
 import ProjectDetail from './src/screens/ProjectDetails';
 import TaskDetailAdmin from './src/screens/TaskScreenAdmin';
+import CalendarScreen from './src/screens/CalendarMenu';
+import ProjectScreen from './src/screens/ProjectScreen';
 
 
 
@@ -8,7 +10,7 @@ const App = () => {
   
   return(
     //Aqui se llaman todos los componentes a mostrar
-    <TaskDetailAdmin/>
+    <CalendarScreen/>
   )
 }
 
